4d9e08bb813edf40b7d98a24456b363736ee0fb7
mac718 <mac718@gmail.com>
2018-11-08 09:07:50 -0500

    Failed attmept to integrate redis and websockets

--- index.js
+++ index.js
@@ -1,16 +1,61 @@
 const express = require('express');
 const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 const redis = require('redis');
 const redisClient = redis.createClient();
+const linkShortener = require('./lib/link-shortener');
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-  redisClient.incr('visitor-count', (err, count) => {
-    res.render('index', {count});
+app.use('/socket.io', express.static(__dirname + 'node_modules/socket.io-client/dist/'));
+
+app.use(express.static(`${__dirname}/public`))
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+var clicks = 0;
+
+io.on('connection', client => {
+  console.log('new connection');
+
+  client.emit('new click', clicks);
+
+  client.on('shortened-link', (link) => {
+    console.log(link)
+    redisClient.hmget('clicks', link, (err, clicks) => {
+      console.log(clicks)
+      clicks++;
+      io.emit('new click', clicks);
+    })
+    
   })
 })
 
-app.listen(3000, 'localhost', () => {
-  console.log('listening...')
+app.get('/', (req, res) => {
+  let result = ''
+  let shortenedLinks;
+  redisClient.hgetall('links', (err, links) =>{
+    
+    redisClient.hgetall('clicks', (err,clicks) => {
+      console.log(clicks)
+
+      res.render('index', {links, clicks, result});
+    })
+  })
 })
+
+app.post('/shorten', (req, res) => {
+  linkShortener.linkShortener(req.body.url)
+    .then((result) => {
+      console.log(result)
+      res.redirect('back')
+    })
+})
+
+// app.listen(3000, 'localhost', () => {
+//   console.log('listening...')
+// })
+
+server.listen(3000);
